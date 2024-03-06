a66242792f4df40a7590f683f79de6c69922d60e
hjck4433 <hjck4433@gmail.com>
2024-03-07 00:20:04 +0900

    intro 추가

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,6 @@
 import { styled } from "styled-components";
+import { useEffect, useState, useRef } from "react";
+import { motion, useAnimation, useScroll } from "framer-motion";
 import introBg from "../images/bg_sky.jpg";
 
 const IntroStyle = styled.section`
@@ -6,20 +8,108 @@ const IntroStyle = styled.section`
   background-size: cover;
   background-attachment: fixed;
   padding-top: 80px;
-  height: 1300px;
-  .container {
+  height: 1800px;
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+
+    .container {
+      padding-top: 100px;
+      padding-bottom: 200px;
+      height: 100%;
+      .textArea {
+        position: sticky;
+        top: 360px;
+        .box {
+          p {
+            color: #fafafa;
+            text-align: center;
+            font-size: 2.6em;
+            &.hello {
+              font-size: 4.8em;
+              font-weight: 600;
+              margin-bottom: 30px;
+            }
+          }
+          &:last-child {
+            position: absolute;
+            top: 0;
+            width: 100%;
+            p {
+              line-height: 1.6;
+            }
+          }
+        }
+      }
+    }
   }
   @media only screen and (max-width: 768px) {
     padding-top: 40px;
+    .wrapper {
+      .container {
+        .textArea {
+          .box {
+            &:last-child {
+              p {
+                font-size: 1.8em;
+                letter-spacing: -1.2px;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 `;
 
 const Intro = () => {
+  const introRef = useRef(null);
+  const [firstBoxOpacity, setFirstBoxOpacity] = useState(1);
+  const [secondBoxOpacity, setSecondBoxOpacity] = useState(0);
+  const { scrollYProgress } = useScroll({ target: introRef });
+
+  useEffect(() => {
+    const updateOpacities = () => {
+      const progress = scrollYProgress.get();
+
+      // Adjust the opacity based on the progress within the Intro component
+      const newFirstBoxOpacity = 1 - Math.min(progress / 0.5, 1);
+      const newSecondBoxOpacity = Math.min((progress - 0.5) / 0.5, 1);
+
+      setFirstBoxOpacity(newFirstBoxOpacity);
+      setSecondBoxOpacity(newSecondBoxOpacity);
+    };
+
+    const unsubscribe = scrollYProgress.onChange(updateOpacities);
+
+    return () => unsubscribe();
+  }, [scrollYProgress]);
+
   return (
     <>
-      <IntroStyle>
-        <div className="container">
-          <div>Intro</div>
+      <IntroStyle ref={introRef}>
+        <div className="wrapper">
+          <div className="container">
+            <div className="textArea">
+              <motion.div
+                className="box"
+                style={{ opacity: firstBoxOpacity }}
+                transition={{ duration: 0.5 }}
+              >
+                <p className="hello">안녕하세요!</p>
+                <p>신입 개발자 김현지 입니다 :)</p>
+              </motion.div>
+              <motion.div
+                className="box"
+                style={{ opacity: secondBoxOpacity }}
+                transition={{ duration: 0.5 }}
+              >
+                <p>기본에 충실한 탄탄한 기술 스택을 구축해나가는,</p>
+                <p>항상 발전하며 성장하는 개발자를 목표로 하고 있습니다.</p>
+              </motion.div>
+            </div>
+          </div>
         </div>
       </IntroStyle>
     </>
--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -7,8 +7,8 @@ import movieverseQr from "../images/movieVerse_qr.jpg";
 
 const projectList = [
   {
-    name: "RepairRun",
-    duration: "2023.10.20 ~ 2023.11.15 (5人)",
+    name: "Cinepic",
+    duration: "2024.01.15 ~ 2024.02.21 (6人)",
     images: [
       `${movieverse1}`,
       `${movieverse2}`,
@@ -21,11 +21,21 @@ const projectList = [
       "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>" +
       "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>",
     function:
-      "빠르게 수선물을 수거하고 처리해주는 검증된 파트너를 매칭해주는 수선 매칭 서비스",
+      "사용자 맞춤 영화 추천/ 독립영화상영극장 정보 제공/ 700여가지의 다양한 영화 정보 제공/ 영화를 좋아하는 사람들의 소통 공간을 제공해주는 서비스 ",
     url: "-",
-    skills: ["HTML", "SCSS", "React", "Spring Boot-JDBC", "Oracle"],
+    skills: [
+      "HTML/",
+      "SCSS/",
+      "React/",
+      "Spring Boot-JPA/",
+      "MySQL/",
+      "Flask/",
+      "scikit-learn/",
+      "Elasticsearch/",
+      "AWS",
+    ],
     notion:
-      "https://hjck4433.notion.site/Project-RepairRun-ae275a8ce16047799facf9aea002175a?pvs=74",
+      "https://hjck4433.notion.site/Project-Cinepic-0980a88679b649f686ae7a6343d87214",
   },
   {
     name: "MovieVerse",
@@ -51,10 +61,51 @@ const projectList = [
       "Flask/",
       "bs4/",
       "AWS/",
-      "Expo/",
+      "Expo",
     ],
     notion:
       "https://hjck4433.notion.site/Project-RepairRun-ae275a8ce16047799facf9aea002175a?pvs=74",
+  },
+  {
+    name: "RepairRun",
+    duration: "2023.10.20 ~ 2023.11.15 (5人)",
+    images: [
+      `${movieverse1}`,
+      `${movieverse2}`,
+      `${movieverse3}`,
+      `${movieverse4}`,
+      `${movieverse5}`,
+    ],
+    text:
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>" +
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>" +
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>",
+    function:
+      "빠르게 수선물을 수거하고 처리해주는 검증된 파트너를 매칭해주는 수선 매칭 서비스",
+    url: "-",
+    skills: ["HTML/", "SCSS/", "React/", "Spring Boot-JDBC/", "Oracle"],
+    notion:
+      "https://hjck4433.notion.site/Project-RepairRun-ae275a8ce16047799facf9aea002175a?pvs=74",
+  },
+  {
+    name: "Blizzard Main / Login (Copy site)",
+    duration: "2023.10.20 ~ 2023.11.15 (1人)",
+    images: [
+      `${movieverse1}`,
+      `${movieverse2}`,
+      `${movieverse3}`,
+      `${movieverse4}`,
+      `${movieverse5}`,
+    ],
+    text:
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>" +
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>" +
+      "<p>리페어런 이란 어쩌고 <b>강조</b>했습니다</p>",
+    function: "디아블로 메인페이지 및 로그인 페이지 카피",
+    url: "https://imaginative-dusk-c76a08.netlify.app/",
+    skills: ["HTML/", "SCSS/", "Javascript"],
+    notion:
+      "https://hjck4433.notion.site/Blizzard-copysite-9476e1d78a4a4703badbd0d0ffbdc205",
   },
 ];
 export default projectList;
